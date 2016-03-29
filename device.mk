9b31b996ed11982fd9b95ce4163f2cc232426427
DerTeufel <dominik-kassel@gmx.de>
2016-03-29 15:57:29 +0200

    Revert "add exfat"
    This reverts commit f8aaf18efb7f23086b33988d8784eec5df289ae6.

--- device.mk
+++ device.mk
@@ -234,9 +234,6 @@ PRODUCT_PACKAGES += \
     mkfs.f2fs \
     make_ext4fs \
     resize2fs \
-    mount.exfat \
-    fsck.exfat \
-    mkfs.exfat \
     setup_fs
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=8
