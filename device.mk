023c80540c4012631d6b257c889d2de8186e22cc
cm <cesar.maximo@gmail.com>
2016-07-05 21:43:25 +0100

    Jiayu Sensor Tool a modified Eng.Mode

--- device.mk
+++ device.mk
@@ -224,6 +224,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Snap
 
+# MediaTek Engineer Menu
+PRODUCT_PACKAGES += \
+        EngineerMode
+
+# MediaTek XLog, to satisfied Eng.Menu dependencies
+# Plus frameworks base Xlog
+PRODUCT_PACKAGES += \
+        libxlog
+
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     e2fsck \
