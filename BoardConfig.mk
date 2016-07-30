c3746f4e264fbad9c7a8ab57bac8684ef2783aa9
DerTeufel <dominik-kassel@gmx.de>
2016-07-30 19:44:05 +0200

    inline-kernel-building
    thx @visi0nary

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,12 +54,19 @@ BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --base 0x40078000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x03f88000 --second_offset 0x00e88000 --tags_offset 0x0df88000 --board Bule
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
-BOARD_CUSTOM_BOOTIMG := true
+#TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
+#BOARD_CUSTOM_BOOTIMG := true
+TARGET_KERNEL_SOURCE := kernel/jiayu/s3_h560
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CONFIG := h560_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+MTK_APPENDED_DTB_SUPPORT := yes
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 # MKImage
 TARGET_MKIMAGE := $(LOCAL_PATH)/mkimage
-TARGET_USE_BUILT_BOOTIMAGE := true
+#TARGET_USE_BUILT_BOOTIMAGE := true
 
 TARGET_KMODULES := true
 # Disable memcpy opt (for audio libraries)
