0ccabc119343d303b8b84f2bac277a5b988b17f2
DerTeufel <dominik-kassel@gmx.de>
2016-08-06 09:45:41 +0200

    Revert "compile hetcomm"
    not working yet
    
    This reverts commit 5617dd0459625cacce58e2467d4dbbaddbba3591.

--- device.mk
+++ device.mk
@@ -217,10 +217,6 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     lights.mt6752
-    
-# Turbo-Download
-PRODUCT_PACKAGES += \
-    HetComm
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
