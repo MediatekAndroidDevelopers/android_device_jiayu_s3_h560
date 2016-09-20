87d8e055534e19d99245273a570bcbedf8d6550b
cm <cesar.maximo@gmail.com>
2016-09-20 17:03:21 +0100

    APNs restore old way of copying since -.- someone broke the new way od copying ... ummm ...

--- full_s3_h560.mk
+++ full_s3_h560.mk
@@ -1,7 +1,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from the common Open Source product configuration
-APNS_CONF := $(LOCAL_PATH)/configs/apns-conf.xml
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
