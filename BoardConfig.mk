83b2ab34a9d293a4e2507fd3b40b170f612e1f72
Alex Kalles <alexakis1997@gmail.com>
2016-11-06 17:09:13 +0200

    Switch to malloc_svelte
    malloc_impl does not exist anymore and
    causes an error during build now
    # Conflicts:
    #	board/device.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,7 +141,9 @@ TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 
 # Enable Minikin text layout engine (will be the default soon)
 USE_MINIKIN := true
-#MALLOC_IMPL := dlmalloc
+
+#Use dlmalloc instead of jemalloc for mallocs
+MALLOC_SVELTE := true
 
 # Fonts
 EXTENDED_FONT_FOOTPRINT := true
