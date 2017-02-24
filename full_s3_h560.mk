cd37ce59b9cbafde6794a8a98b9f6ad09fbbf24d
sandstranger <sandstranger@gmail.com>
2017-02-24 20:48:14 +0400

    inherit some cm features

--- full_s3_h560.mk
+++ full_s3_h560.mk
@@ -7,6 +7,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/jiayu/s3_h560/device.mk)
 
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
 # Release name
 PRODUCT_RELEASE_NAME := s3_h560
 
