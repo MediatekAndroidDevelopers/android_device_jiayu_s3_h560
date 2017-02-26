ca25fa306f4436feb0dbde00b1ae167545524229
sandstranger <sandstranger@gmail.com>
2017-02-26 21:06:44 +0400

    show charge percents during offline charging

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,9 @@ TARGET_BOOTLOADER_BOARD_NAME := mt6752
 # Ashmem
 DISABLE_ASHMEM_TRACKING := true
 BOARD_USES_MTK_HARDWARE := true
+# Charger
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+
 # Kernel
 TARGET_USES_64_BIT_BINDER := true
 TARGET_IS_64_BIT := true
@@ -40,6 +43,10 @@ BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --base 0x40078000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x03f88000 --second_offset 0x00e88000 --tags_offset 0x0df88000 --board 1450352440
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
+
+
+# build old-style zip files (required for ota updater)
+BLOCK_BASED_OTA := false
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 20971520
@@ -95,6 +102,7 @@ TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_DEFAULT_EXTERNAL_STORAGE := true
 endif
 
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 
 # SELinux
