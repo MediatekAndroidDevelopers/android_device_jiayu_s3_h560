2a5fe541b2dba509df78b031b6c1ebedba19f679
sandstranger <sandstranger@gmail.com>
2017-06-18 14:59:40 +0400

    copy file

--- device.mk
+++ device.mk
@@ -53,6 +53,7 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/camera/camerasize.xml:system/etc/camerasize.xml \
 	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
 	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
