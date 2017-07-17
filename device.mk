9af56ce91022d253a1532d30220aa31f80fe6fc6
sandstranger <sandstranger@gmail.com>
2017-07-17 13:12:58 +0400

    update ril class

--- device.mk
+++ device.mk
@@ -111,7 +111,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.kernel.android.checkjni=0 \
-    ro.telephony.ril_class=MT6753 \
+    ro.telephony.ril_class=MediaTekRIL \
     ro.telephony.ril.config=fakeiccid \
     ro.com.android.mobiledata=false
 
