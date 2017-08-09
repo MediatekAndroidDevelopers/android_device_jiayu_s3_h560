04cc9ef2c91d52ad21d6613f2fa7ab67c64da99a
sandstranger <sandstranger@gmail.com>
2017-08-09 19:49:39 +0400

    enable dexpreopt (reverted from commit b963fd979befc9892dc9045bd70a379a36755a4a) (reverted from commit 028026c64042210636b5f37e08c8c15610860390)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,6 +150,9 @@ USE_MINIKIN := true
 
 # Fonts
 EXTENDED_FONT_FOOTPRINT := true
+#dexpreopt
+WITH_DEXPREOPT := true
+WITH_DEXPREOPT_PIC := true
 
 # SELinux
 BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
