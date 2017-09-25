023248992d9842ad39eba29d55618ba39ddfb5b0
sandstranger <sandstranger@gmail.com>
2017-09-25 21:29:01 +0400

    use power hal from Moyster

--- power/Android.mk
+++ power/Android.mk
@@ -1,68 +1,29 @@
-# NOTE: This is based on CM's QCOM power HAL, although basically only the
-# skeleton remains. ;-)
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 LOCAL_PATH := $(call my-dir)
 
-# Hey Mr. Make Author, DIAF PLX
-ifeq ($(TARGET_POWERHAL_VARIANT),mtk-xen0n)
-USE_ME := true
-endif
-
-ifneq (,$(filter true,$(USE_ME)))
-
-# HAL module implemenation stored in
-# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
 
+LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH := hw
-# LOCAL_PROPRIETARY_MODULE := true
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
-LOCAL_SRC_FILES := power.c metadata-parser.c hint-data.c
-
-ifneq ($(BOARD_POWER_CUSTOM_BOARD_LIB),)
-  LOCAL_WHOLE_STATIC_LIBRARIES += $(BOARD_POWER_CUSTOM_BOARD_LIB)
-else
-
-# Include target-specific files.
-#ifeq ($(call is-board-platform-in-list, msm8974), true)
-#LOCAL_SRC_FILES += power-8974.c
-#endif
-
-endif  #  End of board specific list
-
-ifneq ($(TARGET_POWERHAL_SET_INTERACTIVE_EXT),)
-LOCAL_CFLAGS += -DSET_INTERACTIVE_EXT
-LOCAL_SRC_FILES += ../../../../$(TARGET_POWERHAL_SET_INTERACTIVE_EXT)
-endif
+LOCAL_SRC_FILES := power.c
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE_TAGS := optional
 
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
   LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
 endif
 
-ifeq ($(TARGET_POWER_SET_FEATURE_LIB),)
-  LOCAL_SRC_FILES += power-feature-default.c
-else
-  LOCAL_STATIC_LIBRARIES += $(TARGET_POWER_SET_FEATURE_LIB)
-endif
-
-ifneq ($(CM_POWERHAL_EXTENSION),)
-LOCAL_MODULE := power.$(CM_POWERHAL_EXTENSION)
-else
-LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
-endif
-LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
-
-endif # TARGET_POWERHAL_VARIANT == mtk-xen0n
-
-
-# power feature impl for arale
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    power-feature-arale.c \
-
-LOCAL_MODULE := power-feature-arale
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_STATIC_LIBRARY)
