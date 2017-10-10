a64a9b26b046ea0ab0134ec5a14e614fe6c68b43
DerTeufel <dominik-kassel@gmx.de>
2017-10-10 20:09:05 +0200

    device.mk: don't (try to) build libstlport...it's prebuilt now --> vendor

--- device.mk
+++ device.mk
@@ -9,9 +9,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Recovery allowed devices
 TARGET_OTA_ASSERT_DEVICE := JY-S3_ADV,h560,s3_h560,32_h560,s3_32_h560,JY-S3_ADV_32
 
-PRODUCT_PACKAGES += \
-    libstlport
-
 # Manifest
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/manifest.xml:system/vendor/manifest.xml
