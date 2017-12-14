31607778fb7bcba919608d00bb35b7013a3780a6
sandstranger <sandstranger@gmail.com>
2017-12-14 08:05:40 +0400

    use prebuilt kernel in building (reverted from commit 3b22e755db7e510b4593d9c6f3b6ef63ce5879e0)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,7 +46,13 @@ BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive an
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --base 0x40078000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x03f88000 --second_offset 0x00e88000 --tags_offset 0x0df88000 --board 1450352440
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
+TARGET_KERNEL_SOURCE := kernel/jiayu/s3_h560
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CONFIG := h560_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+MTK_APPENDED_DTB_SUPPORT := yes
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 
 # build old-style zip files (required for ota updater)
