445358ba228a0b36bcc119f32e3493dd84985e11
sandstranger <sandstranger@gmail.com>
2017-12-19 22:47:23 +0400

    delete bt vendor (reverted from commit 07d85f7249978d583206c5740b81358e456cee6c) (reverted from commit 9ca89ba4f62e2a28b201117d68f12626a65ed774)

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -2,10 +2,6 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME   "Jiayu S3"
-// Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
-
-
 /* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
 #define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
 #endif
