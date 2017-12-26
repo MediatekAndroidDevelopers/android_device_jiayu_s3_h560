f6a938ce063c329cc790177e0aa16bf767767df4
sandstranger <sandstranger@gmail.com>
2017-12-26 21:56:31 +0400

    cleanup various trash

--- power/Android.mk
+++ power/Android.mk
@@ -15,12 +15,11 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_STATIC_LIBRARIES += power-feature
-
 LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_PROPRIETARY_MODULE:=true
-LOCAL_SRC_FILES := power.c
+LOCAL_SRC_FILES := power.c \
+                   power-feature.c
+
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
 LOCAL_MODULE_TAGS := optional
 
@@ -32,19 +31,3 @@ ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
 endif
 
 include $(BUILD_SHARED_LIBRARY)
-
-# power feature
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    power-feature.c
-
-LOCAL_MODULE := power-feature
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_RELATIVE_PATH := hw
-
-LOCAL_SHARED_LIBRARIES := liblog
-
-LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
-
-include $(BUILD_STATIC_LIBRARY)
