5db0a97163556a7132dc46962376c9bbf643f0ea
Dinolek <dinolek@o2.pl>
2017-12-31 01:19:25 +0100

    libshims: Clean up shim ui and reorganize other shims

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,27 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
-## libshim_ui
+# libshim_gui
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    ui/mtk_ui.cpp \
-    ui/GraphicBuffer.cpp
-
-LOCAL_SHARED_LIBRARIES := libbinder libui liblog libutils libcutils
-LOCAL_MODULE := libshim_ui
-
-LOCAL_C_INCLUDES := \
-    frameworks/native/include \
-    system/core/libutils
-
-LOCAL_CFLAGS := -O3 -Wno-unused-variable -Wno-unused-parameter
-LOCAL_PROPRIETARY_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
-
-
-## libshim_gui
-include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := \
     gui/mtk_gui.cpp \
     gui/SensorManager.cpp
@@ -32,24 +12,28 @@ LOCAL_CFLAGS := -O3 -Wno-unused-variable -Wno-unused-parameter
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
-
-## libshim_snd
+# libshim_snd
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := audio/mtk_audio.cpp
-
 LOCAL_SHARED_LIBRARIES := libbinder libmedia
 LOCAL_MODULE := libshim_snd
 LOCAL_CFLAGS := -O3 -Wno-unused-variable -Wno-unused-parameter
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
-
-## libshim_xlog
+# libshim_ui
 include $(CLEAR_VARS)
+LOCAL_SRC_FILES := ui/mtk_ui.cpp
+LOCAL_SHARED_LIBRARIES := libbinder libui libutils
+LOCAL_MODULE := libshim_ui
+LOCAL_C_INCLUDES := frameworks/native/include
+LOCAL_CFLAGS := -O3 -Wno-unused-variable -Wno-unused-parameter
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
 
+# libshim_xlog
+include $(CLEAR_VARS)
 LOCAL_SRC_FILES := xlog.c
-
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_MODULE := libshim_xlog
 LOCAL_CFLAGS := -O3 -Wno-unused-variable -Wno-unused-parameter
--- libshims/ui/mtk_ui.cpp
+++ libshims/ui/mtk_ui.cpp
@@ -1,5 +1,25 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdint.h>
+#include <sys/types.h>
+
+#include "GraphicBuffer.h"
 #include <ui/GraphicBufferMapper.h>
-#include <ui/Rect.h>
+#include <ui/PixelFormat.h>
 
 extern "C" {
     void _ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handlejRKNS_4RectEP13android_ycbcr(buffer_handle_t, uint32_t, const android::Rect&, android_ycbcr*);
@@ -8,3 +28,55 @@ extern "C" {
         _ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handlejRKNS_4RectEP13android_ycbcr(handle, static_cast<uint32_t>(usage), bounds, ycbcr);
     }
 }
+
+namespace android {
+
+static uint64_t getUniqueId() {
+    static volatile int32_t nextId = 0;
+    uint64_t id = static_cast<uint64_t>(getpid()) << 32;
+    id |= static_cast<uint32_t>(android_atomic_inc(&nextId));
+    return id;
+}
+
+GraphicBuffer::GraphicBuffer()
+    : BASE(), mOwner(ownData), mBufferMapper(GraphicBufferMapper::get()),
+      mInitCheck(NO_ERROR), mId(getUniqueId()), mGenerationNumber(0)
+{
+    width  =
+    height =
+    stride =
+    format =
+    usage  = 0;
+    handle = NULL;
+}
+
+GraphicBuffer::GraphicBuffer(uint32_t inWidth, uint32_t inHeight,
+        PixelFormat inFormat, uint32_t inUsage)
+    : BASE(), mOwner(ownData), mBufferMapper(GraphicBufferMapper::get()),
+      mInitCheck(NO_ERROR), mId(getUniqueId()), mGenerationNumber(0)
+{
+    width  =
+    height =
+    stride =
+    format =
+    usage  = 0;
+    handle = NULL;
+    mInitCheck = initSize(inWidth, inHeight, inFormat, inUsage, "<Unknown>");
+}
+
+GraphicBuffer::GraphicBuffer(uint32_t inWidth, uint32_t inHeight,
+        PixelFormat inFormat, uint32_t inUsage, std::string requestorName)
+    : BASE(), mOwner(ownData), mBufferMapper(GraphicBufferMapper::get()),
+      mInitCheck(NO_ERROR), mId(getUniqueId()), mGenerationNumber(0)
+{
+    width  =
+    height =
+    stride =
+    format =
+    usage  = 0;
+    handle = NULL;
+    mInitCheck = initSize(inWidth, inHeight, inFormat, inUsage,
+            std::move(requestorName));
+}
+
+};
